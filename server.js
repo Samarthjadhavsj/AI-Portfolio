2d72b4f80e420ab9d620ad6cf4194d8acb85f4c7
Samarthjadhavsj <samarthjadhavsj121@gmail.com>
2026-01-04 10:46:55 +0530

    fix: Increase file upload limit to 30MB

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 // Serve static files
 app.use(express.static(__dirname));
